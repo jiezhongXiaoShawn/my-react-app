ce416452c7da4a0d4cd3ed39c9ccc76537b8102b
jiezhongXiaoShawn <112424103+jiezhongXiaoShawn@users.noreply.github.com>
2024-11-12 14:55:30 -0600

    modify board

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
 import { useState } from "react";
+
 function Square() {
+  const [value, setValue] = useState(null);
+
   function handleClick() {
     setValue("X");
   }
-  const [value, setValue] = useState(null);
-  return <button className="square"></button>;
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
+
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
